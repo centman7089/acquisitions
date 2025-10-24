97eb0e6d11956f1931671608c50a7d073b34816e
Centman7089 <innocentimitini@gmail.com>
2025-10-24 10:54:39 +0100

    initial commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import app from './app.js'
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen( PORT, () =>
+{
+    console.log(` app listening on port http://localhost:${PORT}`);
+    
+} )
+
+//"dev": "node --watch index.js" //restart server on file changes

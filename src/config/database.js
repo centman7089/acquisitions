48e3dde8ee26671ae3ef7929db920da704d14c47
Centman7089 <innocentimitini@gmail.com>
2025-10-24 11:57:53 +0100

    Setup up neon postgres with drizzle

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+import dotenv from 'dotenv';
+dotenv.config();
+
+import {neon} from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+const sql = neon( process.env.DATABASE_URL);
+
+const db = drizzle( sql );
+
+export {db, sql};
